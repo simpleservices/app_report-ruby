9c04f8afdac67c540817f6b49752d6c71b5857da
Lucas D'Avila <lucas@lucasdavi.la>
2013-04-06 19:54:25 -0300

    Fix broken test

--- test/app_report/client_test.rb
+++ test/app_report/client_test.rb
@@ -4,19 +4,19 @@ class ClientTest < MiniTest::Unit::TestCase
 
   def test_default_endpoint
     assert_equal \
-      'http://reports.simpleservic.es', 
+      'http://reports.simpleservic.es',
       AppReport::Client.endpoint
   end
 
   def test_connection_return_faraday_connection
-    assert_kind_of Faraday::Connection, AppReport::Client.connection 
+    assert_kind_of Faraday::Connection, AppReport::Client.connection
   end
 
   def test_post_return_faraday_response
     path = '/faraday_response.json'
 
     stub_post(path).to_return \
-      :body    => {}, 
+      :body    => MultiJson.dump({}),
       :headers => { :content_type => "application/json; charset=utf-8" }
 
     assert_kind_of Faraday::Response, AppReport::Client.post(path, {})
@@ -32,7 +32,7 @@ class ClientTest < MiniTest::Unit::TestCase
     path = '/error_messages.json'
 
     stub_post(path).to_return \
-      :body    => fixture('error_messages.json'), 
+      :body    => fixture('error_messages.json'),
       :headers => { :content_type => "application/json; charset=utf-8" }
 
     assert_raises AppReport::Errors::APIError do
